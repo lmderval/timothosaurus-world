fa653d270e4c963955814e3961d42f005bc4d811
louis-mael <louismaelderval@gmail.com>
2024-07-15 11:41:33 +0200

    timothosorus-world: initialize project

---
+++ src/client/java/com/torpill/timothosorus/TimothosorusWorldClient.java
@@ -0,0 +1,10 @@
+package com.torpill.timothosorus;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TimothosorusWorldClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
